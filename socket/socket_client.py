701dd74e7f8765974bba4e62674ba46a4bb97d67
m4o9 <2719211414@qq.com>
2022-07-31 12:31:31 +0800

    new file:   1.py 	new file:   socket/socket_client.py 	new file:   socket/socket_server.py

---
+++ socket/socket_client.py
@@ -0,0 +1,18 @@
+import socket
+import sys
+HOST = "127.0.0.1"
+PORT = 6666
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+try:
+    s.connect((HOST, PORT))
+except Exception as e:
+    print('server not found!')
+    sys.exit()
+while True:
+    c = input('YOU SAY:')
+    s.sendall(c.encode())
+    data = s.recv(1024)
+    data = data.decode()
+    print('Received:', data)
+    if c.lower() == '再见':
+        break
